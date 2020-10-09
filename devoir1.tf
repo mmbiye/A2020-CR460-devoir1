7d12d1bf11d8a3b9cd4212e8269953ef1572f77e
mmbiye <71740364+mmbiye@users.noreply.github.com>
2020-10-09 00:01:54 -0400

    remis de devoir

--- devoir1.tf
+++ devoir1.tf
@@ -1,8 +1,9 @@
+
 provider "google" {
-  project = "monprojetd"
+  project     = "monprojetd"
   credentials = "account.json"
-  region  = "northamerica-northeast1"
-  zone    = "northamerica-northeast1-a"
+  region      = "us-central1"
+  zone        = "us-central1-a"
 }
 
 resource "google_compute_instance" "canard" {
@@ -14,7 +15,6 @@ resource "google_compute_instance" "canard" {
       image = "debian-cloud/debian-9"
     }
   }
-
   network_interface {
     subnetwork = google_compute_subnetwork.prod-dmz.name
     access_config {
@@ -28,7 +28,7 @@ resource "google_compute_instance" "mouton" {
 
   boot_disk {
     initialize_params {
-      image = "coreOs"
+      image = "fedora-coreos-cloud/fedora-coreos-stable"
     }
   }
 
@@ -38,7 +38,7 @@ resource "google_compute_instance" "mouton" {
     }
   }
 
-  }
+}
 
 resource "google_compute_instance" "cheval" {
   name         = "cheval"
@@ -46,7 +46,7 @@ resource "google_compute_instance" "cheval" {
 
   boot_disk {
     initialize_params {
-      image = "coreOs"
+      image = "fedora-coreos-cloud/fedora-coreos-stable"
     }
   }
 
@@ -63,14 +63,16 @@ resource "google_compute_instance" "fermier" {
 
   boot_disk {
     initialize_params {
-      image = "coreOs2004"
+      image = "ubuntu-os-cloud/ubuntu-2004-lts"
     }
   }
 
   network_interface {
-    network = google_compute_network.default.name
+    network = google_compute_network.devoir1.name
     access_config {
     }
+  }
+}
 
 resource "google_compute_network" "devoir1" {
   name                    = "devoir1"
@@ -80,31 +82,30 @@ resource "google_compute_network" "devoir1" {
 resource "google_compute_subnetwork" "prod-dmz" {
   name          = "prod-dmz"
   ip_cidr_range = "172.16.3.0/24"
-  region        = "northamerica-northeast1"
+  region        = "us-central1"
   network       = google_compute_network.devoir1.self_link
 }
 
 resource "google_compute_subnetwork" "prod-interne" {
   name          = "prod-interne"
   ip_cidr_range = "10.0.3.0/24"
-  region        = "northamerica-northeast1"
+  region        = "us-central1"
   network       = google_compute_network.devoir1.self_link
 }
 resource "google_compute_subnetwork" "prod-traitement" {
   name          = "prod-traitement"
   ip_cidr_range = "10.0.2.0/24"
-  region        = "northamerica-northeast1"
+  region        = "us-central1"
   network       = google_compute_network.devoir1.self_link
 }
 
 resource "google_compute_firewall" "ssh" {
   name    = "ssh"
-  network = google_compute_network.prod-traitement.name
+  network = google_compute_network.devoir1.name
   allow {
     protocol = "tcp"
-    ports    = ["2846","5462"]
+    ports    = ["2846", "5462"]
   }
-
 }
 
 resource "google_compute_firewall" "http" {
@@ -116,4 +117,3 @@ resource "google_compute_firewall" "http" {
   }
 
 }
-© 2020 GitHub, Inc.
